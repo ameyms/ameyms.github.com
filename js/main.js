dab904604271bdb2ef855ae2e01ba35e82e0e8ff
Amey Sakhadeo <me@ameyms.com>
2013-09-19 22:57:16 +0530

    Pull higher res profil pic

--- js/main.js
+++ js/main.js
@@ -1,63 +1,57 @@
 var ameyms = {} || ameyms;
 //TODO
 (function(window){
-	
-	var renderLatestPost = function(post)
-	{
-		if(post && post.items && post.items.length > 0)
-		{
-			//console.log(post);
-			$('#latestPost').removeClass('noshow').removeClass('loading');
-			$('#latestPost').html(post.items[0].object.content);
-		}
-	}
+    
+    var renderLatestPost = function(post)
+    {
+        if(post && post.items && post.items.length > 0)
+        {
+            //console.log(post);
+            $('#latestPost').removeClass('noshow').removeClass('loading');
+            $('#latestPost').html(post.items[0].object.content);
+        }
+    }
 
-	var loadLatestPost = function()
-	{
-		var req = gapi.client.plus.activities.list({userId :'101640091246231296580', collection:'public', maxResults:1});
-	  			req.execute(function(resp){
+    var loadLatestPost = function()
+    {
+        var req = gapi.client.plus.activities.list({userId :'101640091246231296580', collection:'public', maxResults:1});
+                req.execute(function(resp){
 
-	  			renderLatestPost(resp);
-	  		});
-	}
-	var renderAboutMe = function(profile)
-	{
-		//console.log(profile);
-		if(profile && profile.aboutMe && profile.image)
-		{
-			$('#gplus-desc').removeClass('loading');
-			$('#gplus-desc').addClass('textReady');
+                renderLatestPost(resp);
+            });
+    }
+    var renderAboutMe = function(profile)
+    {
+        //console.log(profile);
+        if(profile && profile.aboutMe && profile.image)
+        {
+            $('#gplus-desc').removeClass('loading');
+            $('#gplus-desc').addClass('textReady');
 
-			$('#gplus-desc').html(profile.aboutMe);	
-			$('#myPhoto').attr('src', profile.image.url);
+            $('#gplus-desc').html(profile.aboutMe); 
+            $('#myPhoto').attr('src', "https://plus.google.com/s2/photos/profile/101640091246231296580?sz=120");
 
-			loadLatestPost();	
-		}
-		else
-		{
-			$('#gplus-desc').html('Well, I don&apos;t know what to say. But my <a href="https://plus.google.com/101640091246231296580/about">Google+ profile</a> would tell you a thing or two about me.');	
-		}
+            loadLatestPost();   
+        }
+        else
+        {
+            $('#gplus-desc').html('Well, I don&apos;t know what to say. But my <a href="https://plus.google.com/101640091246231296580/about">Google+ profile</a> would tell you a thing or two about me.'); 
+        }
 
-	}
-	window.onGApiLoad = function()
-  	{
-	  	//console.log(gapi);
-	  	
-	  	
-	  	gapi.client.setApiKey('AIzaSyAo7V7FEJisIA_gnS46yAHmjIf7-zeAoyU');
-	  	gapi.client.load('plus', 'v1', function(){
-
-
-	  		  	var req = gapi.client.plus.people.get({userId :'101640091246231296580'});
-	  			req.execute(function(resp){
-
-	  			renderAboutMe(resp);
-	  		});
-	  	});
-	 }
+    }
+    window.onGApiLoad = function()
+    {
+            gapi.client.setApiKey('AIzaSyAo7V7FEJisIA_gnS46yAHmjIf7-zeAoyU');
+            gapi.client.load('plus', 'v1', function(){
+                var req = gapi.client.plus.people.get({userId :'101640091246231296580'});
+                req.execute(function(resp){
+                renderAboutMe(resp);
+            });
+        });
+     }
 
 
-	 ameyms.renderAboutMe = renderAboutMe;
+     ameyms.renderAboutMe = renderAboutMe;
 
 
 
